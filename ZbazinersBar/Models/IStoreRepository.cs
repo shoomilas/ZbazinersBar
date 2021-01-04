3dcc3b5f3ef5393d4134a1b4188c7cb5945fc3ed
Jakub Szumilas <josh.molais@outlook.com>
2021-01-04 21:16:00 +0100

    Start

---
+++ ZbazinersBar/Models/IStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace ZbazinersBar.Models {
+    public interface IStoreRepository {
+
+        IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
+    }
+}
---
+++ ZbazinersBar/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ZbazinersBar.Models;
+
+namespace ZbazinersBar.Models.ViewModels {
+
+    public class ProductsListViewModel {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
