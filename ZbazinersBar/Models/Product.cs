8524f568aab51bf350b1ed6a145ff18afbfc5188
Jakub Szumilas <josh.molais@outlook.com>
2021-01-12 17:36:25 +0100

    Update Product.cs

--- ZbazinersBar/Models/Product.cs
+++ ZbazinersBar/Models/Product.cs
@@ -6,19 +6,19 @@ namespace ZbazinersBar.Models {
     public class Item {
         public long ItemID { get; set; }
 
-        [Required(ErrorMessage = "Please enter a Item name")]
+        [Required(ErrorMessage = "Missing item name")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Please enter a description")]
+        [Required(ErrorMessage = "Missing item description")]
         public string Description { get; set; }
 
         [Required]
         [Range(0.01, double.MaxValue,
-            ErrorMessage = "Please enter a positive price")]
+            ErrorMessage = "Error: Price has to be a positive number")]
         [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }
 
-        [Required(ErrorMessage = "Please specify a category")]
+        [Required(ErrorMessage = "Missing category")]
         public string Category { get; set; }
     }
 }
